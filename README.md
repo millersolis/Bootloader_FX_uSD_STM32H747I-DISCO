2b89f19393e45dd9a731c5021ce7e187dd65bcf3
Miller Solis <millerandressolis@gmail.com>
2023-07-11 17:22:02 -0300

    bootloader proj

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bootloader_FX_uSD_STM32H747I-DISCO
+Basic bootloader using microSD™ file handling with FileX and ThreadX for STM32H747XIH6 MCU using STM32H747I-DISCO board.
+
+Includes sample application project compatible with the bootloader.
---
+++ bootloader/CM7/Debug/Middlewares/FileX/File System/FileX/Core/fx_media_exFAT_format.d
@@ -0,0 +1,13 @@
+Middlewares/FileX/File System/FileX/Core/fx_media_exFAT_format.o: \
+ C:/Users/mille/Sandbox/Bootloader_FX_uSD_STM32H747I-DISCO/bootloader/Middlewares/ST/filex/common/src/fx_media_exFAT_format.c \
+ ../../Middlewares/ST/filex/common/inc/fx_api.h \
+ ../../Middlewares/ST/filex/ports/generic/inc/fx_port.h \
+ ../FileX/App/fx_user.h ../../Middlewares/ST/threadx/common/inc/tx_api.h \
+ ../../Middlewares/ST/threadx/ports/cortex_m7/gnu/inc/tx_port.h \
+ ../Core/Inc/tx_user.h
+../../Middlewares/ST/filex/common/inc/fx_api.h:
+../../Middlewares/ST/filex/ports/generic/inc/fx_port.h:
+../FileX/App/fx_user.h:
+../../Middlewares/ST/threadx/common/inc/tx_api.h:
+../../Middlewares/ST/threadx/ports/cortex_m7/gnu/inc/tx_port.h:
+../Core/Inc/tx_user.h:

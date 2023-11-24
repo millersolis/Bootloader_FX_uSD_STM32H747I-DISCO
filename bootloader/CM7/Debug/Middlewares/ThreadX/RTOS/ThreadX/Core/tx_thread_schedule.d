24c6f9d3cb5bcb66f7aee2749b4610bd60bd46e3
Miller Solis <millerandressolis@gmail.com>
2023-11-23 23:07:07 -0400

    add gitignore

--- bootloader/CM7/Debug/Middlewares/ThreadX/RTOS/ThreadX/Core/tx_thread_schedule.d
+++
@@ -1,2 +0,0 @@
-Middlewares/ThreadX/RTOS/ThreadX/Core/tx_thread_schedule.o: \
- C:/Users/mille/Sandbox/Bootloader_FX_uSD_STM32H747I-DISCO/bootloader/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_schedule.S
--- bootloader/CM7/Debug/Middlewares/ThreadX/RTOS/ThreadX/Core/tx_timer_interrupt.d
+++
@@ -1,2 +0,0 @@
-Middlewares/ThreadX/RTOS/ThreadX/Core/tx_timer_interrupt.o: \
- C:/Users/mille/Sandbox/Bootloader_FX_uSD_STM32H747I-DISCO/bootloader/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_timer_interrupt.S

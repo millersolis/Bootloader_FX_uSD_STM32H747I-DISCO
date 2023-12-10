fa4f5a0325bb5a37b561f916f07d75d798f8d394
Miller Solis <millerandressolis@gmail.com>
2023-12-10 19:13:34 -0400

    FileX for uSD (#5)

---
+++ bootloader/CM7/Debug/Core/Src/fw_update.d
@@ -0,0 +1,9 @@
+Core/Src/fw_update.o: ../Core/Src/fw_update.c ../Core/Inc/fw_update.h \
+ ../Core/Inc/sd_card.h ../../Middlewares/ST/threadx/common/inc/tx_api.h \
+ ../../Middlewares/ST/threadx/ports/cortex_m7/gnu/inc/tx_port.h \
+ ../Core/Inc/tx_user.h
+../Core/Inc/fw_update.h:
+../Core/Inc/sd_card.h:
+../../Middlewares/ST/threadx/common/inc/tx_api.h:
+../../Middlewares/ST/threadx/ports/cortex_m7/gnu/inc/tx_port.h:
+../Core/Inc/tx_user.h:
--- bootloader/CM7/Debug/Core/Src/subdir.mk
+++ bootloader/CM7/Debug/Core/Src/subdir.mk
@@ -6,37 +6,46 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../Core/Src/app_threadx.c \
+../Core/Src/fw_update.c \
 ../Core/Src/main.c \
+../Core/Src/sd_card.c \
 ../Core/Src/stm32h7xx_hal_msp.c \
 ../Core/Src/stm32h7xx_hal_timebase_tim.c \
 ../Core/Src/stm32h7xx_it.c \
 ../Core/Src/syscalls.c \
-../Core/Src/sysmem.c 
+../Core/Src/sysmem.c \
+../Core/Src/utils.c 
 
 S_UPPER_SRCS += \
 ../Core/Src/tx_initialize_low_level.S 
 
 OBJS += \
 ./Core/Src/app_threadx.o \
+./Core/Src/fw_update.o \
 ./Core/Src/main.o \
+./Core/Src/sd_card.o \
 ./Core/Src/stm32h7xx_hal_msp.o \
 ./Core/Src/stm32h7xx_hal_timebase_tim.o \
 ./Core/Src/stm32h7xx_it.o \
 ./Core/Src/syscalls.o \
 ./Core/Src/sysmem.o \
-./Core/Src/tx_initialize_low_level.o 
+./Core/Src/tx_initialize_low_level.o \
+./Core/Src/utils.o 
 
 S_UPPER_DEPS += \
 ./Core/Src/tx_initialize_low_level.d 
 
 C_DEPS += \
 ./Core/Src/app_threadx.d \
+./Core/Src/fw_update.d \
 ./Core/Src/main.d \
+./Core/Src/sd_card.d \
 ./Core/Src/stm32h7xx_hal_msp.d \
 ./Core/Src/stm32h7xx_hal_timebase_tim.d \
 ./Core/Src/stm32h7xx_it.d \
 ./Core/Src/syscalls.d \
-./Core/Src/sysmem.d 
+./Core/Src/sysmem.d \
+./Core/Src/utils.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -48,7 +57,7 @@ Core/Src/%.o: ../Core/Src/%.S Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/app_threadx.cyclo ./Core/Src/app_threadx.d ./Core/Src/app_threadx.o ./Core/Src/app_threadx.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32h7xx_hal_msp.cyclo ./Core/Src/stm32h7xx_hal_msp.d ./Core/Src/stm32h7xx_hal_msp.o ./Core/Src/stm32h7xx_hal_msp.su ./Core/Src/stm32h7xx_hal_timebase_tim.cyclo ./Core/Src/stm32h7xx_hal_timebase_tim.d ./Core/Src/stm32h7xx_hal_timebase_tim.o ./Core/Src/stm32h7xx_hal_timebase_tim.su ./Core/Src/stm32h7xx_it.cyclo ./Core/Src/stm32h7xx_it.d ./Core/Src/stm32h7xx_it.o ./Core/Src/stm32h7xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/tx_initialize_low_level.d ./Core/Src/tx_initialize_low_level.o
+	-$(RM) ./Core/Src/app_threadx.cyclo ./Core/Src/app_threadx.d ./Core/Src/app_threadx.o ./Core/Src/app_threadx.su ./Core/Src/fw_update.cyclo ./Core/Src/fw_update.d ./Core/Src/fw_update.o ./Core/Src/fw_update.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/sd_card.cyclo ./Core/Src/sd_card.d ./Core/Src/sd_card.o ./Core/Src/sd_card.su ./Core/Src/stm32h7xx_hal_msp.cyclo ./Core/Src/stm32h7xx_hal_msp.d ./Core/Src/stm32h7xx_hal_msp.o ./Core/Src/stm32h7xx_hal_msp.su ./Core/Src/stm32h7xx_hal_timebase_tim.cyclo ./Core/Src/stm32h7xx_hal_timebase_tim.d ./Core/Src/stm32h7xx_hal_timebase_tim.o ./Core/Src/stm32h7xx_hal_timebase_tim.su ./Core/Src/stm32h7xx_it.cyclo ./Core/Src/stm32h7xx_it.d ./Core/Src/stm32h7xx_it.o ./Core/Src/stm32h7xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/tx_initialize_low_level.d ./Core/Src/tx_initialize_low_level.o ./Core/Src/utils.cyclo ./Core/Src/utils.d ./Core/Src/utils.o ./Core/Src/utils.su
 
 .PHONY: clean-Core-2f-Src
 
